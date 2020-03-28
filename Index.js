4c5bd34a12506f4475b68a80eea5601764b4bc61
Falk <petruk0908@gmail.com>
2020-03-28 18:39:12 +0300

    edit readme

--- Index.js
+++ Index.js
@@ -1,8 +1,17 @@
 const GraphBuilder = require('./classes/GraphBuilder');
 // указываем число потоков
-let treadCount = process.argv[3]? process.argv[3] > 10 ? 10 : process.argv[3] : 4;
+let treadCount = process.argv[3] ? process.argv[3] > 10 ? 10 : process.argv[3] : 4;
 let graph = new GraphBuilder(treadCount);
 // указываем ссылку на ресурс для анализа
 
 //https://www.anilibria.tv/
-graph.runAnalize(process.argv[2]);
+if (process.argv[2]) {
+    try {
+        graph.runAnalize(process.argv[2]);
+    } catch (err) {
+        console.log(err);
+    }
+
+} else {
+    console.log('введите полный url сайта');
+}
